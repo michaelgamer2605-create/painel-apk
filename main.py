38f76c5c007a5cb07ccf3b1aa0f0b6a16e882612
michaelsz7 <michaelgamer2605@gmail.com>
2026-03-31 14:53:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-
-class Painel(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(orientation='vertical', **kwargs)
-
-        self.add_widget(Label(text="PAINEL OK 🔥"))
 
 class MeuApp(App):
     def build(self):
-        return Painel()
+        return Label(text="OK 🔥")
 
 if __name__ == "__main__":
     MeuApp().run()
