4d43ce26b0bc8be53aa3cd919f76eea674435ff3
michaelsz7 <michaelgamer2605@gmail.com>
2026-03-31 14:44:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.switch import Switch
+from kivy.uix.label import Label
 
-class Painel(App):
+class Painel(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(orientation='vertical', **kwargs)
+
+        self.add_widget(Label(text="PAINEL ADMIN 🔥"))
+
+        self.add_widget(Label(text="Aimbot"))
+        self.add_widget(Switch())
+
+        self.add_widget(Label(text="ESP"))
+        self.add_widget(Switch())
+
+        self.add_widget(Label(text="Wallhack"))
+        self.add_widget(Switch())
+
+class MeuApp(App):
     def build(self):
-        return Button(text="Painel Funcionando 🔥")
+        return Painel()
 
-Painel().run()
+MeuApp().run()
