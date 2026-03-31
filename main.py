0e280ed97a2bc106eea1f409750ba65dbd061a8d
michaelsz7 <michaelgamer2605@gmail.com>
2026-03-31 14:52:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,12 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.switch import Switch
 from kivy.uix.label import Label
-from kivy.graphics import Color, Rectangle
-
-class Linha(BoxLayout):
-    def __init__(self, texto, **kwargs):
-        super().__init__(orientation='horizontal', size_hint_y=None, height=50, **kwargs)
-
-        self.add_widget(Label(text=texto, color=(1,1,1,1)))
-        self.add_widget(Switch())
 
 class Painel(BoxLayout):
     def __init__(self, **kwargs):
-        super().__init__(orientation='vertical', padding=10, spacing=10, **kwargs)
+        super().__init__(orientation='vertical', **kwargs)
 
-        # Fundo preto
-        with self.canvas.before:
-            Color(0, 0, 0, 1)
-            self.rect = Rectangle(size=self.size, pos=self.pos)
-
-        self.bind(size=self.update_rect, pos=self.update_rect)
-
-        # Título
-        self.add_widget(Label(text="PAINEL PRO 🔥", font_size=24, color=(1,0,1,1)))
-
-        # Seções
-        self.add_widget(Label(text="AIM", color=(1,0,1,1)))
-        self.add_widget(Linha("Aimbot"))
-        self.add_widget(Linha("Aim Suave"))
-
-        self.add_widget(Label(text="ESP", color=(1,0,1,1)))
-        self.add_widget(Linha("ESP Linha"))
-        self.add_widget(Linha("ESP Caixa"))
-        self.add_widget(Linha("ESP Nome"))
-        self.add_widget(Linha("ESP Vida"))
-
-        self.add_widget(Label(text="MISC", color=(1,0,1,1)))
-        self.add_widget(Linha("Velocidade"))
-        self.add_widget(Linha("Pulo Alto"))
-
-    def update_rect(self, *args):
-        self.rect.size = self.size
-        self.rect.pos = self.pos
+        self.add_widget(Label(text="PAINEL OK 🔥"))
 
 class MeuApp(App):
     def build(self):
